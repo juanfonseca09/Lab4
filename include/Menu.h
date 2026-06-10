9d8273d6ea0bbaa3005a45e2a51cd042bb4c7f8c
CabreraLuciano <luciano1cabrera@gmail.com>
2026-06-10 15:11:20 -0300

    Centralizar la fabrica y manejadores, alta usuario y alta viaje completados y probados

--- include/Menu.h
+++ include/Menu.h
@@ -1,8 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
+#include "IControladorUsuario.h"
+#include "IControladorFechaActual.h"
+#include "IControladorReserva.h"
+#include "IControladorCalificacion.h"
 
 class Menu {
 private:
+    IControladorUsuario* controladorUsuario;
+    IControladorFechaActual* controladorFecha;
+    IControladorReserva* controladorReserva;
+    IControladorCalificacion* controladorCalificacion;
+
     void altaUsuario();
     void altaViaje();
     void generarReserva();
@@ -12,6 +21,8 @@ private:
     void cargarDatos();
 
 public:
+    Menu(IControladorUsuario* ctrlUsuario, IControladorFechaActual* ctrlFecha, IControladorReserva* ctrlReserva, IControladorCalificacion* ctrlCalificacion);
+    ~Menu();
     void mostrarMenu();
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "../include/Menu.h"
+#include "../include/Fabrica.h"
 
 int main() {
-    Menu menu;
+    Fabrica* fabrica = Fabrica::getInstance();
+    IControladorUsuario* ctrlUsuario = fabrica->getIControladorUsuario();
+    IControladorFechaActual* ctrlFecha = fabrica->getIControladorFechaActual();
+    IControladorReserva* ctrlReserva = fabrica->getIControladorReserva();
+    IControladorCalificacion* ctrlCalificacion = fabrica->getIControladorCalificacion();
+
+    Menu menu(ctrlUsuario, ctrlFecha, ctrlReserva, ctrlCalificacion);
+    // precargar datos (opcional)
     menu.mostrarMenu();
     return 0;
 }
